321a6047b537a368fe9e97ab7238be44445e05cb
NielzosFilms <nielzosfilms@gmail.com>
2024-07-22 22:27:35 +0200

    use eclipse as IDE

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,18 @@
 #include "SDL2/SDL.h"
 #include "iostream"
 
-int main(int argc, char** argv) {
-    std::cout << "simple-cpp-renderer#main()\n";
+int main(int argc, char **argv) {
+	std::cout << "simple-cpp-renderer#main()\n";
 
-    SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Window* _window = SDL_CreateWindow("Simple c++ renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_OPENGL);
+	SDL_Window *_window = SDL_CreateWindow("Simple c++ renderer",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400,
+		SDL_WINDOW_OPENGL);
 
-    std::cout << "Enter any key to quit...\n";
-    int a;
-    std::cin >> a;
+	std::cout << "Enter any key to quit...\n";
+	int a;
+	std::cin >> a;
 
-    return 0;
+	return 0;
 }
